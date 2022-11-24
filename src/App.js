099c8c8a1e9316d0d82573608415a0786334d0c3
rahulrn15 <727721eucs114@skcet.ac.in>
2022-11-24 12:12:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <div>
+            <ul className="navbar">
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <div className="class">hi</div>
+        </div>
+     );
+}
+export default Navbar;
